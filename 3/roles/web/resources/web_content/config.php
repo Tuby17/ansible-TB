c2c541dbf9f27558c71f1f746948b57711e29c9b
Tuboly Bálint <tu-by@hotmail.com>
2022-06-06 16:06:21 +0200

    20220606

---
+++ 3/roles/web/resources/web_content/config.php
@@ -0,0 +1,4 @@
+<?php
+
+$Username = "tubyadmin";
+$Password = password_hash('myn3wp4ssw0rd1ssup3rs3cure', PASSWORD_DEFAULT);
---
+++ 3/roles/web/resources/web_content/index.php
@@ -0,0 +1,41 @@
+<?php
+
+  session_start();
+
+  if($_SESSION['Active'] == false){
+    header("location:login.php");
+	  exit;
+  }
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <title>Logged in</title>
+  </head>
+  <body>
+    <div class="container">
+
+      <div class="jumbotron">
+        <h1>Status: logged in</h1>
+        <p class="lead">flag{d0ntcr055th3str34ms!}</p>
+        <p><a class="btn btn-lg btn-success" href="logout.php" role="button">Log out</a></p>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; Mario Font 2016</p>
+      </footer>
+
+    </div>
+
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
